d9700386c8c02dfbcedf3ec7bb4252816ed7ebdd
Md FaizurRahman <114854264+Faizurcse@users.noreply.github.com>
2024-04-20 16:10:21 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,15 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  console.log('Request Received')
   // TODO: Send the ""Hello, world!"" response
+  res.write('Hello, world!')
+  res.end()
 });
 
 server.listen(port, () => {
   // TODO: Log a message to the console when the server starts listening on the port
+  console.log('starts listening on the port')
 });
 
 // DO NOT EDIT BELOW THIS LINE
